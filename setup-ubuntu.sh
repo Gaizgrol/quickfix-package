595cb068e2908392afb1a87c7cba98d6250bafd7
Gabriel Izoton <gabrielizotongo@gmail.com>
2024-12-11 08:41:03 -0300

    Setup sem interação de usuário

--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -1,6 +1,4 @@
-apt-get install git
-apt-get install doxygen
-apt-get install graphviz
+apt-get -y install git doxygen graphviz libtool g++ ruby ruby-dev python python-dev libpq-dev
 
 # Autoconf precisa ser a versão 2.71
 wget --no-check-certificate https://ftp.gnu.org/gnu/autoconf/autoconf-2.71.tar.xz
@@ -12,11 +10,3 @@ make install
 . ~/.bashrc
 cd ..
 rm -rf autoconf-2.71
-
-apt-get install libtool
-apt-get install g++
-apt-get install ruby
-apt-get install ruby-dev
-apt-get install python
-apt-get install python-dev
-apt-get install libpq-dev
